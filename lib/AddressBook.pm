d165bfcb6c241dd0d034d30fa5b506cb73df8665
Anton Ukolov <antonuk84@yandex.ru>
2010-11-23 19:30:19 +0300

    begin to set up db

--- lib/AddressBook.pm
+++ lib/AddressBook.pm
@@ -1,14 +1,33 @@
 package AddressBook;
+
 use Dancer ':syntax';
 use Template;
+use DBI;
+use Carp;
+use File::Slurp qw(read_file);
 use utf8;
+
+### Subs
+
+sub init_db {
+    my $db = connect_db();
+
+    my $schema = read_file( setting("db_sch") )
+        or carp( "no sql schema" );
+    $db->do( $schema ) 
+        or carp $db->errstr;
+}
+
+sub connect {
+    my $dbh = DBI->connect( setting("db_dbi") . setting("db_name") )
+        or carp $DBI->errstr;
+
+    return $dbh;
+}
+### Handlers
 
 get '/' => sub {
     template 'home';
-};
-
-get '/donate/thanks' => sub {
-    template 'thanks';
 };
 
 true;
---
+++ schema.sql
@@ -0,0 +1,6 @@
+create table if not exists entries (
+    id integer primary key autoincrement,
+    title string not null,
+    text string not null
+);
+
